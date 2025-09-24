353ddc146761e28c27aacb332935c9b031c4b71e
herry123435 <herry123435@gmail.com>
2025-09-24 14:35:52 +0000

    Deploying to gh-pages from @ herry123435/herry123435.github.io@0ef84cd9c770af9a31357f27695d6f8c77fa179c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -424,6 +424,16 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-ai-tech-play",
+          title: 'AI Tech Play',
+          description: "Instructed students on the development of an autonomous vehicle within the MIT Racecar environment.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-learning-dynamics-of-an-autonomous-vehicle",
+          title: 'Learning dynamics of an autonomous vehicle',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
             },},{
         id: 'social-email',
         title: 'email',
